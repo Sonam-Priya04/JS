eee19e76792947e00218f97b8210a2f954f496ea
Sonam Priya <sonamjha0409@gmail.com>
2025-03-07 15:14:25 +0000

    conversion

---
+++ 01_basics/3_conversion.js
@@ -0,0 +1,31 @@
+//string to number
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
+
+//"33" = 33
+//"33abc" = NaN
+//true = 1;false = 0
+
+// string to boolean,number to boolean
+let logged = "sonam"
+let boollogged = Boolean(logged)
+console.log(typeof boollogged)
+console.log(boollogged)
+
+//1 = true
+//0 = false
+//"" = false
+//"string" = true
+
+// number to string
+
+let someNumber = 23
+console.log(typeof someNumber)
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber)
